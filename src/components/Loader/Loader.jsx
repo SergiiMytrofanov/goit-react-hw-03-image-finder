089da34f5fe38528f98e07ce375e1c643588fb5a
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-08-06 19:21:01 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Vortex } from 'react-loader-spinner'
 
 const Loader = () => {
-  return <div className="Loader">Loading...</div>;
+  return <div className="Loader"><Vortex
+  visible={true}
+  height="80"
+  width="80"
+  ariaLabel="vortex-loading"
+  wrapperStyle={{}}
+  wrapperClass="vortex-wrapper"
+  colors={['#107980', '#76A1A7', '#EDE1CF', '#D9C5BC', '#B6ADAF', '#66347F', '#fe777d', '#fecdc7']}
+/></div>;
 };
 
 export default Loader;

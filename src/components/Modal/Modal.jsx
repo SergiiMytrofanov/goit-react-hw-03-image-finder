90f67dd46e5ea5e28cb432be6b6b314ad8a64082
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-08-06 23:06:20 +0300

    update
    fixed styles

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,30 +1,28 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import * as basicLightbox from 'basiclightbox';
 import styles from './Modal.module.css';
 
 const Modal = ({ largeImageURL, onClose }) => {
-  useEffect(() => {
-    const modalInstance = basicLightbox.create(`
+  const modalInstance = basicLightbox.create(`
+    <div class=${styles.Overlay}>
       <div class=${styles.Modal}>
         <img src="${largeImageURL}" alt="Large Image" />
       </div>
-    `);
+    </div>
+  `);
 
-    const handleEscapeKey = (event) => {
-      if (event.key === 'Escape') {
-        modalInstance.close();
-      }
-    };
+  const handleEscapeKey = (event) => {
+    if (event.key === 'Escape') {
+      modalInstance.close();
+    }
+  };
 
-    modalInstance.show();
+  modalInstance.show();
 
-    window.addEventListener('keydown', handleEscapeKey);
-    return () => {
-      window.removeEventListener('keydown', handleEscapeKey);
-    };
-  }, [largeImageURL]);
-
-  return null;
+  window.addEventListener('keydown', handleEscapeKey);
+  return () => {
+    window.removeEventListener('keydown', handleEscapeKey);
+  };
 };
 
 export default Modal;
